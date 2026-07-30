e9bcf59e61ce3124b3b452cd22dff0d2580df885
赵萍萍 <zhaopingping531@163.com>
2026-07-30 16:34:18 +0800

    Extract Supreme Court case list

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from turtle import title
+
 import requests
 from bs4 import BeautifulSoup
 
 from sources import SOURCES
+
+from urllib.parse import urljoin
 
 
 headers = {
@@ -32,4 +36,21 @@ for source in SOURCES:
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    print("网页标题：", soup.title.text.strip())
+    article_list = soup.find("div", class_="sec_list")
+    #print(article_list)
+   
+    articles = article_list.find("ul").find_all("li", recursive=False)
+
+    print("文章数量：", len(articles))
+
+    for article in articles:
+        link_tag = article.find("a")
+
+        title = link_tag.text.strip()
+        date = article.find("i", class_="date").text.strip()
+        link = urljoin(source["url"], link_tag["href"])
+
+        print(title)
+        print(date)
+        print(link)
+        print("-" * 40)
