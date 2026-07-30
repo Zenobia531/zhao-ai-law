e98dcb7e18d655281a3c08a0866949a235a43eb0
Zenobia531 <Zhaopingping531@163.com>
2026-07-30 11:58:39 +0800

    Create sources.py
    Add Supreme People's Court source

---
+++ sources.py
@@ -0,0 +1,8 @@
+SOURCES = [
+    {
+        "name": "中华人民共和国最高人民法院",
+        "column": "指导性案例",
+        "url": "https://www.court.gov.cn/fabu/gengduo/151.html",
+        "enabled": True,
+    }
+]
