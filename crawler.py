94740efbbd6f84fb8a696a2b7ab18e9b614e73a8
Zenobia531 <Zhaopingping531@163.com>
2026-07-30 12:13:24 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -36,3 +36,24 @@ def fetch_page_title(url: str) -> str:
         return soup.title.string.strip()
 
     return "未提取到网页标题"
+    
+def download_html(url):
+    import requests
+
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/138.0 Safari/537.36"
+        )
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=20,
+    )
+
+    response.raise_for_status()
+
+    return response.text
